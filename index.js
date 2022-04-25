06cc50f7ebbdb87d0eee7c88a66ab8f1b0bdb26c
chupakabrito <elchupakabrito@gmail.com>
2022-04-25 01:10:50 +0100

    fix backticks on index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function chupashadows(options) {
     }
 
     images.forEach(image => {
-        image.style.boxShadow = '10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)';
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
         
         if(options.padding) {
             image.style.padding = '1em';
